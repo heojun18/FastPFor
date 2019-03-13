7d07816d4b6bf0707a9203b7190ef24a62f59862
heojun18 <heojun18@gmail.com>
2019-03-13 17:38:38 +0900

    cleanup and add script

---
+++ execute.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+make clean
+
+make -j8
+
+make codecs
+
+rm test1.log
+
+#./codecs --codecs pfor --needtodelta --zipfian1
+./codecs --codecs iiu --needtodelta --zipfian1
+#./codecs --codecs pfor,iiu --needtodelta --zipfian1
